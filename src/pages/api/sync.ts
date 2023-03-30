78cb8f3abd6238a1cf855781603a4c648f96769a
wasu <wevorence@gmail.com>
2023-03-31 01:19:46 +0900

    fetch失敗時に全部落ちてたから修正

--- src/pages/api/sync.ts
+++ src/pages/api/sync.ts
@@ -3,6 +3,17 @@ import { PrismaClient } from "@prisma/client";
 
 const client = new PrismaClient();
 
+async function safeFetch(url: string) {
+  let status = false;
+  try {
+    const result = await fetch(url);
+    status = result.status === 200;
+  } catch (e) {
+    console.log(e);
+  }
+  return status;
+}
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
@@ -10,16 +21,25 @@ export default async function handler(
   if (req.method === "GET") {
     // 全サービスの状態を取得。
     const srvs = await client.webService.findMany();
-    srvs.forEach(async (srv) => {
-      const result = await fetch(srv?.url);
-      const status = result.status === 200;
+    let total_status = true;
+    let failed_list = [];
+    for (let i = 0; i < srvs.length; i++) {
+      let srv = srvs[i];
+      const status = await safeFetch(srv?.url);
       await client.webServiceStatus.create({
         data: {
           webServiceId: srv?.id,
-          status: status,
+          status,
         },
       });
-    });
-    res.status(200).json({ msg: "success" });
+      total_status = total_status && status;
+      if (!status) failed_list.push(srv?.name);
+    }
+    if (total_status) {
+      res.status(200).json({ msg: "success" });
+    } else {
+      res.status(400).json({ msg: "failed", failed_list });
+    }
+    return;
   }
 }
